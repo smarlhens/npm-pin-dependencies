e1c36b00a8e65a998b04af6115135fbc5eca4ab3
Samuel MARLHENS <samuel.marlhens@gmail.com>
2025-05-08 13:57:10 +0200

    chore: update ncu config

---
+++ .ncurc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  reject: [],
+  target: name => {
+    const targets = {
+      '@types/node': 'minor',
+      /**
+       * v3 requires node >=20, keeping v2.x
+       */
+      'sort-package-json': 'minor',
+      /**
+       * v6 requires node >=20, keeping v5.x
+       */
+      rimraf: 'minor',
+      /**
+       * breaking changes in v9
+       */
+      execa: 'minor',
+      /**
+       * breaking changes in v5
+       */
+      knip: 'minor',
+    };
+
+    const keys = Object.keys(targets);
+    if (keys.some(key => new RegExp(key).test(name))) {
+      return targets[keys.find(key => new RegExp(key).test(name))];
+    }
+
+    return 'latest';
+  },
+};
